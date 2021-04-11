f283492e4a31dd80616f64c18d96cbb6472dc67b
hj <xhfl7195@gmail.com>
2021-04-11 16:47:06 +0900

    Add AuthorInfo Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import AuthorInfo  from './AuthorInfo'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AuthorInfo/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
