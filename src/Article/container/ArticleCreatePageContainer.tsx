8cabe52aade96d6968eb76980706221bd44eac84
Derek-94 <ghdtjrrl94@gmail.com>
2021-04-25 14:41:11 +0900

    feat(ArticleCreatePageContainer): 구조 변경.
    - 컨테이너 컴포넌트 구조 변경.

--- src/Article/container/ArticleCreatePageContainer.tsx
+++ src/Article/container/ArticleCreatePageContainer.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import ArticleCreatePage from "../presentational/ArticleCreatePage";
 
 const ArticleCreatePageContainer = () => {
-    const onEvent = () => {
-        
+    const onRegisterArticle = () => {
+        // api 호출로 post 날리고
+        // ArticleDetail 조회 페이지로 이동.
     }
 
     return (
-        <ArticleCreatePage />
+        <ArticleCreatePage 
+            onRegisterArticle = {onRegisterArticle}
+        />
     )
 }
+
+export default ArticleCreatePageContainer;
