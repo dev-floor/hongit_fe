1e0273680c026e12d6789b1b1ec850fd93a5e08e
dayonxxi <62105312+dayonxxi@users.noreply.github.com>
2021-05-02 12:24:14 +0900

    feature(게시물 상세 조회): 게시물 상세 페이지 바디 컴포넌트 개발 (#15)
    - ArticleBody부분 content, image, hashtags 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import AuthorInfoContainer from './Article/container/AuthorInfoContainer';
+import ArticleBodyContainer from './Article/container/ArticleBodyContainer';
 import ArticleHeaderContainer from './Article/container/ArticleHeaderContainer';
 import CommentContainer from './Article/container/CommentContainer';
 import ArticleCreatePage from './Article/presentational/ArticleCreatePage';
@@ -9,9 +10,10 @@ import ArticleCreatePage from './Article/presentational/ArticleCreatePage';
 function App() {
   return (
     <div className="App">
-      {/* <ArticleHeaderContainer />
+      <ArticleHeaderContainer />
+      <ArticleBodyContainer />
       <AuthorInfoContainer/>
-      <CommentContainer/> */}
+      <CommentContainer/> 
       <ArticleCreatePage />
     </div>
   );
