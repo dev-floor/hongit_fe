1593355e2f57ec1b11fd9f0cd58b56fec25a84d3
Derek-94 <ghdtjrrl94@gmail.com>
2021-05-04 16:36:10 +0900

    feat(router): Add router funtions
    - Go back when cancel registering article
    - Go foward to article Detail when register article

--- src/Article/container/ArticleCreatePageContainer.tsx
+++ src/Article/container/ArticleCreatePageContainer.tsx
@@ -1,38 +1,23 @@
 import React from 'react';
-<<<<<<< HEAD
 import { RouteComponentProps } from 'react-router-dom';
-=======
->>>>>>> ac57c5badb2ca50f8aa50f5ad2a7e6983ded1bdb
 import ArticleCreatePage from '../presentational/ArticleCreatePage';
 import { articleAPI } from '../../api/api';
 import { ArticleCreateApi } from '../../api/ApiProps';
 
-<<<<<<< HEAD
 interface MatchParams {
   id: string;
 }
 
 const ArticleCreatePageContainer = ({ match, history }: RouteComponentProps<MatchParams>) => {
-=======
-const ArticleCreatePageContainer = () => {
->>>>>>> ac57c5badb2ca50f8aa50f5ad2a7e6983ded1bdb
   const onRegisterArticle = (newArticle: ArticleCreateApi) => {
     // api 호출로 post 날리고
     articleAPI.postArticle(newArticle);
     // ArticleDetail 조회 페이지로 이동.
-<<<<<<< HEAD
-    history.goBack();
+    history.push("/Detail")
     console.log(newArticle);
   };
 
   return <ArticleCreatePage onRegisterArticle={onRegisterArticle} history = {history} />;
-=======
-
-    console.log(newArticle);
-  };
-
-  return <ArticleCreatePage onRegisterArticle={onRegisterArticle} />;
->>>>>>> ac57c5badb2ca50f8aa50f5ad2a7e6983ded1bdb
 };
 
 export default ArticleCreatePageContainer;
