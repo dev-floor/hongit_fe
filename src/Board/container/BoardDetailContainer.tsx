ba2edf4ce49579de8e34b0a2306739645d712845
hj <xhfl7195@gmail.com>
2021-05-30 14:15:08 +0900

    Merge

--- src/Board/container/BoardDetailContainer.tsx
+++ src/Board/container/BoardDetailContainer.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import ArticleCreatePageContainer from '../../Article/container/ArticleCreatePageContainer';
 import ArticleListContainer from '../../ArticleList/container/ArticleListContainer';
 import BoardContentHeader from '../presentational/BoardContentHeader';
 import { boardAPI } from '../../api/api';
+import { BoardDetailApi, BoardOption } from '../../api/ApiProps';
 
 import 'css/BoardDetail.css';
 
 const BoardDetailContainer = () => {
-  const datas = boardAPI.get();
+  const [boardData, setboardData] = useState<BoardDetailApi>({
+    id:0,
+    name:'',
+    professorName:'',
+    subjectName:'',
+    boardTypeName:'',
+    boardTypeText:'',
+    dividedOptions:[] as BoardOption[],
+    articleTypeOptions:[] as BoardOption[],
+  });
+
+  const loadData = async () => {
+    const response = await boardAPI.get(/* boardId */);
+    setboardData(response);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="board-detail">
-      <div className="side-bar"> side-bar 영역 </div>
+      <div className="side-bar"> side-bar </div>
       <div className="vertical"> </div>
       <div className="content">
         <div className="contents-header">
-          <BoardContentHeader boardDetailData={datas} />
+          <BoardContentHeader boardDetailData={boardData} />
         </div>
         <hr />
+        <div>
+      {boardData.dividedOptions.map((option) => (
+          <button className="hashtag" type="button">
+            {option.text}
+          </button>
+        ))}
+  {boardData.articleTypeOptions.map((option) => (
+          <button className="hashtag" type="button">
+            {option.text}
+          </button>
+        ))}
+      </div>
         <div className="article-area">
-          <ArticleListContainer />
+          <ArticleListContainer/>
         </div>
       </div>
+      <Route path="/board/write" component={ArticleCreatePageContainer} exact />
     </div>
   );
 };
--- src/Board/presentational/BoardContentHeader.tsx
+++ src/Board/presentational/BoardContentHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BoardDetailProp } from '../argumentsPropsInterface/BoardProps';
 
 const BoardContentHeader = ({ boardDetailData }: BoardDetailProp) => {
@@ -8,7 +9,7 @@ const BoardContentHeader = ({ boardDetailData }: BoardDetailProp) => {
       <div className="board-name">{boardDetailData.name}</div>
       <div className="board-button-area">
         <button type="button">필터</button>
-        <button type="button">글쓰기</button>
+        <Link to='/write'><button type="button">글쓰기</button></Link>
       </div>
     </div>
   );
