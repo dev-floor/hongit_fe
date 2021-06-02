2321698572954b850e92e19744d167441c1bb5ef
hj <xhfl7195@gmail.com>
2021-06-02 23:11:57 +0900

    Apply format & Add Comment

--- src/ArticleList/container/ArticleListContainer.tsx
+++ src/ArticleList/container/ArticleListContainer.tsx
@@ -3,8 +3,7 @@ import { articleAPI } from 'api/api';
 import ArticleListArea from '../presentational/ArticleList';
 
 const ArticleListContainer = () => {
-  const articleListData = articleAPI.getByList();
-
+  const articleListData = articleAPI.getByList(); // 여기에 BoardId를 전달하여 List를 받아오고
   return <ArticleListArea articleListData={articleListData} />;
 };
 
