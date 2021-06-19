20b5f00fdd97d1078b457a34a0a6936a2131bf89
HJ <80870154+hyunju95@users.noreply.github.com>
2021-06-19 17:46:05 +0900

    feature(공통 컴포넌트) : 좋아요 버튼 컴포넌트 구현 (#56)
    * Add Toggle Button Component
    
    * Button Css Modify
    
    * Tgl Btn Variable Name changed
    
    * Css Changed
    
    * Press State Naming Changed

---
+++ src/Commons/TglBtn.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHeart,
+  faQuestionCircle,
+  faTag,
+} from '@fortawesome/free-solid-svg-icons';
+
+import 'css/TglBtn.css';
+
+const IconByType = (props: any) => {
+  const { type, press } = props;
+  const defaultIcon = {
+    heart: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faHeart}
+        style={{ color: 'lightgray' }}
+      />
+    ),
+    wonder: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faQuestionCircle}
+        style={{ color: 'lightgray' }}
+      />
+    ),
+    scrap: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faTag}
+        style={{ color: 'lightgray' }}
+      />
+    ),
+  };
+  const pressIcon = {
+    heart: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faHeart}
+        style={{ color: 'salmon' }}
+      />
+    ),
+    wonder: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faQuestionCircle}
+        style={{ color: 'yellowgreen' }}
+      />
+    ),
+    scrap: (
+      <FontAwesomeIcon
+        className="tgl-btn-icon"
+        icon={faTag}
+        style={{ color: 'skyblue' }}
+      />
+    ),
+  };
+  return <div>{press ? pressIcon[type] : defaultIcon[type]}</div>;
+};
+
+const TglBtn = (props: any) => {
+  const { type, count, handler } = props;
+  const [pressState, setPress] = useState<boolean>(false);
+  const [curCount, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    setCount(count);
+  }, [count]);
+
+  const onToggleButton = () => {
+    setPress(!pressState);
+    if (!pressState) {
+      setCount(curCount + 1);
+    } else {
+      setCount(curCount - 1);
+    }
+    handler();
+  };
+
+  return (
+    <div>
+      <button type="button" className="tgl-btn" onClick={onToggleButton}>
+        <IconByType type={type} press={pressState} /> {curCount}
+      </button>
+    </div>
+  );
+};
+export default TglBtn;
