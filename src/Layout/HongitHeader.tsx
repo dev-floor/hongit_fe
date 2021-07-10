6b2a66a27c2448206e6ef321dce9483bf1abd785
dayonxxi <ydl1230@naver.com>
2021-07-10 16:32:31 +0900

    remove not needed import

--- src/Layout/HongitHeader.tsx
+++ src/Layout/HongitHeader.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useRecoilValue, useResetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { viewBanner } from 'Atoms/atom';
 import { Segment } from 'semantic-ui-react';
 
